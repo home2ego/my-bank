ea29561b4abbaf519190398eb56d0c4b2e53a21d
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-14 22:02:11 +0200

    MyBank app

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                onboarding: resolve(__dirname, "pages/onboarding.html"),
+                dashboard: resolve(__dirname, "pages/dashboard.html"),
+                profile: resolve(__dirname, "pages/profile.html"),
+                services: resolve(__dirname, "pages/services.html"),
+                signin: resolve(__dirname, "pages/signin.html"),
+                transactions: resolve(__dirname, "pages/transactions.html"),
+            },
+        },
+    },
+});
